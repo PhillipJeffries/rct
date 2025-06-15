f8464203d76c44155e3f0668aec0970ab5c7cc9e
dmitry <ggbox@mail.ru>
2025-06-15 20:20:41 +0400

    fix image name

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import { classNames } from "shared/lib/helpers/classNames/classNames"
 import * as s from './Navbar.module.scss'
 import AppLink, { AppLinkTheme } from "shared/ui/AppLink/AppLink"
 import { useTranslation } from "react-i18next"
+import { useTheme } from "App/providers/ThemeProvider"
 
 const links = [
     { name: 'домой', to: '/' },
@@ -9,12 +10,13 @@ const links = [
 ]
 
 const Navbar = () => {
+    const {theme} = useTheme()
     const { t } = useTranslation()
     return (
         <div className={classNames(s.navbar)}>
             {
                 links.map(({ name, to }) => (
-                    <AppLink theme={AppLinkTheme.SECONDARY} to={to} key={to}>
+                    <AppLink theme={theme ===  'dark' ? AppLinkTheme.SECONDARY : AppLinkTheme.PRIMARY} to={to} key={to}>
                         {t(name)}
                     </AppLink>
                 ))
